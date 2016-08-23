1fb5e57bf68568604f7480213aa1192b40ff458c
Sparkz333 <MWarrander@gmail.com>
2016-08-24 01:54:17 +0930

    improved grass clicking and added animation. Added from to fighting/catching column - not functional as of yet-

--- js/_jquery.js
+++ js/_jquery.js
@@ -1,10 +1,29 @@
+// Exploring / Clicking Grass Grass
 var explore = 0;
-var exploreInc = 2.5;
+var exploreInc = 10;
+var clickDelay = false;
 
 $("#grassTile").mousedown(function(){
     var $exploreBar = $("#exploreBar");
     
-    explore += exploreInc;
-    $exploreBar.css({"width" : explore + "%",});
-    //$exploreBar.text(explore);
+    //udpate explore progress only when not animated
+    if (clickDelay === false){
+        explore += exploreInc;
+        $exploreBar.css({"width" : explore + "%",});
+        //$exploreBar.text(explore);
+    }
+    
+    //Add shake animation to grass tile when 100% explore
+    if (explore >= 100) {
+        clickDelay = true;
+        $(this).addClass('animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $(this).removeClass("animated shake");
+            explore = 0;
+            $exploreBar.css({"width" : explore + "%",});
+            clickDelay = false;
+        });;
+    };
 });
+
+// Random Picklemon
+
